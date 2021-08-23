a68a90ab9ac2c748da96aec67f13e8bef3f7030e
rkmitra1 <69916391+rkmitra1@users.noreply.github.com>
2021-08-23 15:43:01 -0500

    convert content to structure

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,84 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+const Articles = [
+  {
+    title: 'Figure 8 knot - free hand',
+    steps: ['bight - facing up', 'over and under', 'through the front'],
+  },
+  {
+    title: 'Clove hitch',
+    steps: [
+      'bight - right over left',
+      'second bight - right over left',
+      'second bight - being first',
+      'clip in',
+      'pull tight',
+    ],
+  },
+  {
+    title: 'Barrel Knot',
+    steps: [
+      'left hand - hold rope facing right',
+      'wrap around hand - three times',
+      'take end under all three loops - left to right',
+    ],
+  },
+
+  {
+    title: 'Double Fisherman knot',
+    steps: ['create loop', 'or connect two same size ropes'],
+  },
+  {
+    title: 'Prusik Hitch',
+    steps: ['use loop'],
+  },
+  {
+    title: 'Bowline - fixed',
+    steps: [
+      'knot',
+      'end trough loop',
+      'end around attachment',
+      'follow rope back around knot',
+    ],
+  },
+  {
+    title: 'Bowline - one hand',
+    steps: [
+      'bight up',
+      'bight around both standing lines',
+      'pull through loop',
+    ],
+  },
+  {
+    title: 'Girth Hitch',
+    steps: ['bight around attachment', 'pull through loop'],
+  },
+  {
+    title: 'Taut Line Hitch',
+    steps: [
+      'bight in rope',
+      'pull bight from bottom rope throuch bight',
+      'tighten up',
+      'pull bottom through created loop',
+      'pull to tighten',
+      'lock by: pull free end through loop, two half hitches',
+    ],
+  },
+  {
+    title: 'Ashley’s Stopper Knot',
+    steps: [
+      'Take end, loop, and tie knot around working line,',
+      'pull free end through the loop',
+      'tighten up',
+    ],
+  },
+  {
+    title: 'Double Overhand Stopper Knot',
+    steps: ['overhand bight', 'pull end around bight twice', 'pull to tighten'],
+  },
+]
+
 const HomePage = () => {
   return (
     <div className="prose min-w-full bg-gray-200">
@@ -10,80 +88,23 @@ const HomePage = () => {
         /* you should un-comment description and add a unique description, 155 characters or less
         You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
       />
-      <h1 className="text-center">Raj's Rope Knots &amp; Hitches</h1>
+      <h1 className="text-center bg-gray-100 text-white py-2">
+        Raj's Rope Knots &amp; Hitches
+      </h1>
 
-      <div className="md:w-3/4 mx-auto bg-white px-4 py-1 md:px-24">
-        <h2 className="border-t-2 border-blue-500">
-          Figure 8 knot - free hand
-        </h2>
-        <ul>
-          <li>bight - facing up</li>
-          <li>over and under</li>
-          <li>through the front</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Figure 8 follow thru</h2>
-        <ul>
-          <li>bight - facing down</li>
-          <li>over</li>
-          <li>around back</li>
-          <li>over and through loop</li>
-          <li>end through biner</li>
-          <li>follow rope back</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Clove hitch</h2>
-        <ul>
-          <li>bight - right over left</li>
-          <li>second bight - right over left</li>
-          <li>second bight - being first</li>
-          <li>clip in</li>
-          <li>pull tight</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Barrel Knot</h2>
-        <ul>
-          <li>left hand - hold rope facing right</li>
-          <li>wrap around hand - three times</li>
-          <li>take end under all three loops - left to right</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Double Fisherman knot</h2>
-        <ul>
-          <li>create loop</li>
-          <li>or connect two same size ropes</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Prusik Hitch</h2>
-        <ul>
-          <li>use loop</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Bowline - fixed</h2>
-        <ul>
-          <li>bight down -left over right</li>
-          <li>end trough loop - from back</li>
-          <li>end around attachment</li>
-          <li>follow rope around knot</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Bowline - one hand</h2>
-        <ul>
-          <li></li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Girth hitch</h2>
-        <ul>
-          <li>uses loop</li>
-          <li>loop around post</li>
-          <li>pull through loop</li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Taut Line hitch</h2>
-        <ul>
-          <li></li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">Ashley's Stopper knot</h2>
-        <ul>
-          <li></li>
-        </ul>
-        <h2 className="border-t-2 border-blue-500">
-          Double Overhand Stopper knot
-        </h2>
-        <ul>
-          <li></li>
-        </ul>
+      <div className=" md:w-3/4 mx-auto bg-white px-4 pt-2 md:px-16">
+        {Articles.map((article) => (
+          <>
+            <h2 className="border-t-2 border-blue-500">{article.title}</h2>
+            <div className="pb-1">
+              {article.steps.map((step) => (
+                <ul className="mx-auto w-4/5">
+                  <li className="">{step}</li>
+                </ul>
+              ))}
+            </div>
+          </>
+        ))}
       </div>
     </div>
   )
